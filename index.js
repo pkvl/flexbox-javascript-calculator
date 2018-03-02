28a9153490228342132d792c1664d76e89cbed1d
anroypaul <pavlo.koval@techs.com.ua>
2018-03-01 21:56:03 -0800

    mouse click event

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+function transitionLogic(e) {
+
+    textField = document.getElementById('text-field');
+    textField.innerHTML = "";
+
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => item.addEventListener('transitionend', removeTransition));
+}
+
 function addTransition(e) {
     changeValue(e);
     const item = document.querySelector(`.item[data-key="${e.keyCode}"]`);
     if (!item) return;
     item.classList.add('pressed');
+}
+
+function addClickTransition(e) {
+    if (!e.target.classList.contains('item')) return;
+    e.target.classList.add('pressed');
+
+    const keyCode = parseInt(e.target.getAttribute('data-key'));
+    const key = actionsCodes.includes(parseInt(e.target.getAttribute('data-key'))) ? e.target.innerHTML : parseInt(e.target.innerHTML);
+
+    let button = { key, keyCode };
+    changeValue(button);
 }
 
 function removeTransition(e) {
@@ -11,27 +31,27 @@ function removeTransition(e) {
 }
 
 function changeValue(e) {
-    if(e.keyCode === clearKeyCode) {
+    if (e.keyCode === clearKeyCode) {
         operatorOne = symbol = operatorTwo = null;
         textField.innerHTML = "";
     }
 
-    if(e.keyCode === resultKeyCode) {
-        if(operatorOne !== null) getResult();
+    if (e.keyCode === resultKeyCode) {
+        if (operatorOne !== null) getResult();
         return;
     }
 
-    if(actionsCodes.includes(e.keyCode)) {
+    if (actionsCodes.includes(e.keyCode)) {
         selectAction(e.key);
         return;
     }
-    
-    if(e.keyCode === removeKeyCode) {
+
+    if (e.keyCode === removeKeyCode) {
         textField.innerHTML = textField.innerHTML.slice(0, -1);
         return;
     }
-    
-    if(digitsCodes.includes(e.keyCode)) {
+
+    if (digitsCodes.includes(e.keyCode)) {
         textField.innerHTML += e.key;
         return;
     }
@@ -60,16 +80,6 @@ const resultKeyCode = 13;
 const clearKeyCode = 46;
 const removeKeyCode = 8;
 
-window.onload = function(e) {
-
-    textField = document.getElementById('text-field');
-    textField.innerHTML = "";
-
-    const items = document.querySelectorAll('.item');
-    items.forEach(item => item.addEventListener('transitionend', removeTransition));
-};
-
+window.onload = transitionLogic;
 window.addEventListener('keydown', addTransition);
-window.addEventListener('mousedown', function(e) {
-    console.log(e.target);
-});
+window.addEventListener('mousedown', addClickTransition);
